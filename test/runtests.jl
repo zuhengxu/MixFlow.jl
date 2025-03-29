02028781170e36c0b9a1e75e8d2a28d7aed46824
Zuheng <zuhengdavid@gmail.com>
2025-03-28 18:27:22 -0700

    add more stab tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,51 +13,58 @@ include("Model.jl")
 include("mfvi.jl")
 using Test
 
-target = load_model("Banana")
-
-target_ad = ADgradient(AutoMooncake(; config = Mooncake.Config()), target)
-reference, _ = mfvi(target_ad; sample_per_iter = 10, max_iters = 10000)
-prob = MixFlowProblem(reference, target_ad)
-
-dim = LogDensityProblems.dimension(target_ad)
-
-T_max = 200
-mixer = RandomShift(dim, T_max)
 
 @testset "check invertibility" begin
-    @testset "$K" for K in [
-        uncorrectHMC(10, 0.02),
-        HMC(10, 0.02),
-        MALA(0.25),
-        RWMH(0.3 * ones(dim)),
+    @testset "$(name)" for name in [
+        "Banana",
+        "Cross",
+        "Funnel",
+        "WarpedGaussian",
     ]
+        target = load_model(name)
 
-    T = 20
-    
-    x0, v0, uv0, ua0 = MF._rand_joint_reference(prob, K)
-    x, v, uv, ua = x0, v0, uv0, ua0
+        target_ad = ADgradient(AutoMooncake(; config=Mooncake.Config()), target)
+        reference, _ = mfvi(target_ad; sample_per_iter=10, max_iters=10000)
+        prob = MixFlowProblem(reference, target_ad)
 
-    rejs_fwd = []
-    for t in 1:T
-        x, v, uv, ua, acc = MF.forward(prob, K, mixer, x, v, uv, ua, t)
-        if !acc 
-            push!(rejs_fwd, t)
+        dim = LogDensityProblems.dimension(target_ad)
+
+        T_max = 200
+        mixer = RandomShift(dim, T_max)
+        @testset "$K" for K in [
+            uncorrectHMC(10, 0.02),
+            HMC(10, 0.02),
+            MALA(0.25),
+            RWMH(0.3 * ones(dim)),
+        ]
+
+            T = 60
+
+            x0, v0, uv0, ua0 = MF._rand_joint_reference(prob, K)
+            x, v, uv, ua = x0, v0, uv0, ua0
+
+            rejs_fwd = []
+            for t in 1:T
+                x, v, uv, ua, acc = MF.forward(prob, K, mixer, x, v, uv, ua, t)
+                if !acc
+                    push!(rejs_fwd, t)
+                end
+            end
+
+            rejs_inv = []
+            for t in T:-1:1
+                x, v, uv, ua, acc = MF.inverse(prob, K, mixer, x, v, uv, ua, t)
+                if !acc
+                    push!(rejs_inv, t)
+                end
+            end
+            rejs_inv = sort(rejs_inv)
+
+            # ensure that the forward and inverse steps are consistent
+            # @test rejs_fwd .- rejs_inv ≈ zeros(length(rejs_fwd))
+
+            @test norm(x .- x0) ≤ 1e-3
+            @test norm(v .- v0) ≤ 1e-3
         end
-    end
-
-    rejs_inv = []
-    for t in T:-1:1
-        x, v, uv, ua, acc = MF.inverse(prob, K, mixer, x, v, uv, ua, t)
-        if !acc
-            push!(rejs_inv, t)
-        end
-    end
-    rejs_inv = sort(rejs_inv)
-
-    # ensure that the forward and inverse steps are consistent
-    # @test rejs_fwd .- rejs_inv ≈ zeros(length(rejs_fwd))
-
-    @test norm(x .- x0) ≤ 1e-3
-    @test norm(v .- v0) ≤ 1e-3
     end
 end
