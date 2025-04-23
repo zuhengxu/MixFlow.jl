6cadfed64276f8797f76e6e534e0e5eb138a728b
Zuheng <zuhengdavid@gmail.com>
2025-04-22 21:12:26 -0700

    push the new traces res

---
+++ example/compare_traces/work/3a/49f7bb2d17a47049a47d61ad3137f6/.command.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env julia --threads=5 --project=/home/zuheng/Research/MixFlow.jl/example
+
+include("/home/zuheng/Research/MixFlow.jl/example/compare_traces/traces.jl")
+
+# get configurations
+seed = 9
+name = "Cross"
+tracetype = "bwd_irf"
+kernel_type = MF.MALA
+
+# run simulation
+df = run_traces(seed, name, kernel_type, tracetype)
+
+# store output
+mkdir("seed=9___tracetype=bwd_irf___target=Cross___kernel=MF.MALA")
+CSV.write("seed=9___tracetype=bwd_irf___target=Cross___kernel=MF.MALA/summary.csv", df)
---
+++ example/compare_traces/work/bb/05d512c3032518afb2a8722d31848c/.command.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env julia --threads=5 --project=/home/zuheng/Research/MixFlow.jl/example
+
+include("/home/zuheng/Research/MixFlow.jl/example/compare_traces/traces.jl")
+
+# get configurations
+seed = 9
+name = "Cross"
+tracetype = "bwd_inv_irf"
+kernel_type = MF.HMC
+
+# run simulation
+df = run_traces(seed, name, kernel_type, tracetype)
+
+# store output
+mkdir("seed=9___tracetype=bwd_inv_irf___target=Cross___kernel=MF.HMC")
+CSV.write("seed=9___tracetype=bwd_inv_irf___target=Cross___kernel=MF.HMC/summary.csv", df)
