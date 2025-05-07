531699f3917b4a45c14b3517f9de725b2e4ae8f4
Zuheng <zuhengdavid@gmail.com>
2025-05-06 22:13:09 -0700

    update rwmh implementation for better stability

--- src/kernel/rwmh.jl
+++ src/kernel/rwmh.jl
@@ -1,35 +1,30 @@
-using PDMats: PDiagMat
-
 struct RWMH{T} <: MultivariateInvolutiveKernel 
-    σs::PDiagMat{T} # diagonal stds of the proposal
+    ϵ::T # stepsize
+    invdiagM::AbstractVector{T} # diagonal preconditioner
 end
 
-RWMH(σs::AbstractVector{T}) where {T} = RWMH(PDiagMat(σs))
-# ϵ as stepsize, and σs as the diagonal preconditioning
-RWMH(ϵ::T, σs::AbstractVector{T}) where {T} = RWMH(PDiagMat(ϵ .* σs))
-RWMH(dim::Int) = RWMH(PDiagMat(ones(dim)))
-
-_involution(::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}, v::AbstractVector{T}) where T = (v, x)
-_dist_v_given_x(K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}) where T = MvNormal(x, K.σs.^2)
-_rand_v_given_x(K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}) where T = randn(length(x)) .* K.σs.diag .+ x
-_rand_v_given_x(K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}, n::Int) where T = randn(length(x), n) .* K.σs.diag .+ x
+_dimension(K::RWMH) = length(K.invdiagM)
+_dist_v_given_x(K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}) where T = MvNormal(zeros(_dimension(K)), I)
+_rand_v_given_x(K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}) where T = randn(_dimension(K)) 
 
 _cdf_v_given_x(
     K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}, v::AbstractVector{T}
-) where T = normcdf.(x, K.σs.diag, v)
+) where T = normcdf.(v)
 
 _invcdf_v_given_x(
     K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}, uv::AbstractVector{T}
-) where T = norminvcdf.(x, K.σs.diag, uv)
+) where T = norminvcdf.(uv)
 
-# normcdf(μ, σ, x)
-# norminvcdf(μ, σ, x)
+_involution(
+    K::RWMH{T}, ::MixFlowProblem, x::AbstractVector{T}, v::AbstractVector{T}
+) where T = (x .+ K.ϵ .* v .* K.invdiagM, -v)
+
 
 function _rand_joint_reference(prob::MixFlowProblem, K::RWMH)
-    dim = LogDensityProblems.dimension(prob.target)
+    dims = LogDensityProblems.dimension(prob.target)
     x = rand(prob.reference)
     v = _rand_v_given_x(K, prob, x)
-    uv = rand(dim)
+    uv = rand(dims)
     ua = rand()
     return x, v, uv, ua
 end
@@ -41,20 +36,3 @@ function mcmc_step(prob::MixFlowProblem, K::RWMH{T}, x::AbstractVector{T}) where
     logr = logdensity_target(prob, v) - logdensity_target(prob, x)
     return check_acc(rand(), logr) ? v : x
 end
-
-
-# function rwmh_sampler(
-#     prob::MixFlowProblem, K::RWMH{T}, x0::AbstractVector{T}, nsteps::Int,
-# ) where {T<:Real}
-#     # Initialize the chain
-#     x = x0
-#     samples = Vector{T}(undef, length(x0), nsteps)
-#     samples[:, 1] .= x
-
-#     for i in 1:nsteps
-#         # Propose a new sample
-#         x = rwmh(prob, K, x)
-#         samples[:, i] .= x
-#     end
-#     return samples
-# end
